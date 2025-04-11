23e088fc8676269c0bef97933bc38a65470cbb40
Lukasz Madrzak <l.madrzak@gmail.com>
2025-04-11 23:08:29 +0700

    no message

--- convertfast.rb
+++ convertfast.rb
@@ -1,6 +1,6 @@
 cask "convertfast" do
   version "1.0.0"
-  sha256 "PLACEHOLDER_SHA256" # This will need to be updated with the actual SHA256 of the release
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/madrzak/ConvertFast/releases/download/v#{version}/ConvertFast-#{version}.zip"
   name "ConvertFast"
